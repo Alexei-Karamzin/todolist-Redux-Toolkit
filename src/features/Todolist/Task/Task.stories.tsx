bb699315d68f8ff8a3512e78be5e29f37f0a262c
Alexei Karamzin <1999aleks_1999@mail.ru>
2022-11-02 22:49:04 +0300

    fix bug storybook task component

--- src/features/Todolist/Task/Task.stories.tsx
+++ src/features/Todolist/Task/Task.stories.tsx
@@ -5,6 +5,7 @@ import {action} from "@storybook/addon-actions";
 import {Task} from "./Task";
 import {v1} from "uuid";
 import {TaskPriority} from "../../../api/tasks-api";
+import {ReduxStoreProviderDecorator} from "../../../App/ReduxStoreProviderDecorator";
 
 export default {
     title: 'TODOLIST/Task',
@@ -14,7 +15,8 @@ export default {
         changeTaskStatus: action('changeTaskStatus'),
         changeTaskTitle: action('changeTaskTitle'),
         removeTask: action('removeTask')
-    }
+    },
+    decorators: [ReduxStoreProviderDecorator],
 } as ComponentMeta<typeof Task>;
 
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
@@ -23,7 +25,7 @@ export const TaskIsDoneStory = Template.bind({});
 export const TaskIsNotDoneStory = Template.bind({});
 
 TaskIsDoneStory.args = {
-    task: {id: v1(), title: 'HTML&CSS', description: '', todoListId: 'todolistId1', order: 0, status: 1, priority: TaskPriority.Low, startDate: '', deadline: '', addedDate: ''}
+    task: {id: v1(), title: 'HTML&CSS', description: '', todoListId: 'todolistId1', order: 0, status: 2, priority: TaskPriority.Low, startDate: '', deadline: '', addedDate: ''}
 };
 
 TaskIsNotDoneStory.args = {
